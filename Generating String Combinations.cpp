14c536cb0decfacc6d64e98e558ca598dfe214cf
MASESHASAAI241031 <241031.ec@rmkec.ac.in>
2026-06-05 22:13:40 +0530

    Update Generating String Combinations.cpp

--- Generating String Combinations.cpp
+++ Generating String Combinations.cpp
@@ -9,21 +9,21 @@ void solve() {
     
     int L = s.length();
     
-    // Outer loop: Count from 1 up to (2^L) - 1
+    
     for(int ctr = 1; ctr < (1 << L); ctr++) {
         
         string combi = "";
         
-        // Inner loop: Check every possible letter position
+        
         for(int sh = 0; sh < L; sh++) {
             
-            // Check if the 'sh' switch is currently flipped ON inside our counter
+            
             if((ctr & (1 << sh)) > 0) {
-                combi += s[sh]; // If ON, add the letter to our combination
+                combi += s[sh]; 
             }
         }
         
-        cout << combi << endl; // Print the generated combination
+        cout << combi << endl; 
     }
 }
 
