2e11b0ef3195e08bc8f4623ee247f0ee25149738
MASESHASAAI241031 <241031.ec@rmkec.ac.in>
2026-06-12 12:07:03 +0530

    Add files via upload

---
+++ linear_substring.cpp
@@ -0,0 +1,27 @@
+#include iostream
+#include string
+#include vector
+using namespace std;
+
+int main() {
+    string a, b;
+    cin  a  b;
+    
+    int m = 0;
+    vectorvectorint dp(a.length() + 1, vectorint(b.length() + 1, 0));
+    
+    for (int i = 1; i = a.length(); i++) {
+        for (int j = 1; j = b.length(); j++) {
+            if (a[i - 1] == b[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (m  dp[i][j]) {
+                    m = dp[i][j];
+                }
+            }
+        }
+    }
+    
+    cout  m  endl;
+    
+    return 0;
+}
